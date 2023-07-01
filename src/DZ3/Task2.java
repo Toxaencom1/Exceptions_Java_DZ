b1d7d40dc076e35d683c329cfa596a66fb4103a6
Toxaencom1 <Toxaencom@gmail.com>
2023-07-01 18:36:18 +0600

    Solved second task for DZ3

--- src/DZ3/Task2.java
+++ src/DZ3/Task2.java
@@ -1,4 +1,5 @@
 package DZ3;
+
 /* Задача №2
 По желанию
 Вы разрабатываете систему банковских транзакций. Необходимо написать программу,
@@ -15,4 +16,141 @@ package DZ3;
 При успешном переводе, сумма должна списываться с текущего счета и зачисляться на целевой счет.
  */
 public class Task2 {
+    public static Transaction makeTransaction(BankAccount from, BankAccount to, double money) {
+        System.out.print("Transaction: from '" + from.getName() + "' amount: " + money +
+                "$ to '" + to.getName() + "' processing...   ");
+        Transaction transaction = new Transaction(from, to, money);
+        try {
+            if (transaction.executeTransaction()) { // boolean executeTransaction() бросает ошибки
+                System.out.print("<<Transaction complete!>>\n");
+                return transaction;
+            }
+        } catch (InsufficientFundsException | InvalidAmountException e) {
+            System.out.print("<<Failed!>>   " + e.getMessage());
+        }
+        return transaction;
+    }
+
+    public static void main(String[] args) {
+        BankAccount peter = new BankAccount("Peter Capaldi", 2000.0);
+        System.out.println(peter);
+        BankAccount john = new BankAccount("John Doe");
+        System.out.println(john);
+        System.out.println();
+        Transaction f1 = makeTransaction(peter, john, 567.0); // Examples
+        Transaction f2 = makeTransaction(peter, john, 2000.0);
+        Transaction f3 = makeTransaction(peter, john, -765.0);
+        Transaction f4 = makeTransaction(peter, john, 0.0);
+        Transaction f5 = makeTransaction(john, peter, 100.0);
+        Transaction f6 = makeTransaction(john, peter, 567.0);
+        System.out.println("\n" + peter); // Total balance
+        System.out.println(john);
+        System.out.println("\nResults:"); //results
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(f3);
+        System.out.println(f4);
+        System.out.println(f5);
+        System.out.println(f6);
+    }
+}
+
+class BankAccount {
+    String holderName;
+    double money;
+
+    BankAccount(String holderName) {
+        this.holderName = holderName;
+        this.money = 0;
+    }
+
+    BankAccount(String holderName, double money) {
+        this.holderName = holderName;
+        this.money = money;
+    }
+
+    public String getName() {
+        return holderName;
+    }
+
+    public void setName(String holderName) {
+        this.holderName = holderName;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    public void addMoney(double money) {
+        if (money > 0) {
+            this.money += money;
+        }
+    }
+
+    public void reduceMoney(double money) {
+        if (money > 0 && money < this.money) {
+            this.money -= money;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount: " +
+                "Holder Name is '" + holderName + '\'' +
+                ", with balance: " + money + "$";
+    }
+}
+
+class Transaction {
+    private final BankAccount firstCustomer;
+    private final BankAccount secondCustomer;
+    private final double firstCustomerHaveMoney;
+    private final double moneyQuantity;
+    private boolean status;
+
+    public Transaction(BankAccount from, BankAccount to, double moneyQuantity) {
+        this.firstCustomer = from;
+        this.secondCustomer = to;
+        this.firstCustomerHaveMoney = firstCustomer.getMoney();
+        this.moneyQuantity = moneyQuantity;
+        this.status = false;
+    }
+
+
+    public boolean executeTransaction() throws InsufficientFundsException, InvalidAmountException {
+        if (moneyQuantity <= 0) {
+            throw new InvalidAmountException("Transfer amount is incorrect!\n");
+        }
+        if (moneyQuantity > firstCustomerHaveMoney) {
+            throw new InsufficientFundsException(firstCustomer.getName() + " has not enough money!\n");
+        }
+        firstCustomer.reduceMoney(moneyQuantity);
+        secondCustomer.addMoney(moneyQuantity);
+        this.status = true;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        if (status){
+            return "Confirmed";
+        }else return "Rejected";
+
+    }
+}
+
+class InsufficientFundsException extends Exception {
+    public InsufficientFundsException(String message) {
+        super(message);
+    }
+}
+
+class InvalidAmountException extends Exception {
+    public InvalidAmountException(String message) {
+        super(message);
+    }
 }
